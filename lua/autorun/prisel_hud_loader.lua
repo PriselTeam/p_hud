3a9a9d6dcce088fa793670750820451e542f20e2
Ekali <pcnlmmm@gmail.com>
2024-06-11 15:59:23 +0200

    first commit

---
+++ lua/autorun/prisel_hud_loader.lua
@@ -0,0 +1,14 @@
+PHud = {}
+
+local function I(sFile) return include("p_hud/"..sFile) end
+local function A(sFile) return AddCSLuaFile("p_hud/"..sFile) end
+local function IA(sFile) return I(sFile), A(sFile) end
+
+
+if SERVER then
+    A("client/functions.lua")
+    A("client/hooks.lua")
+else
+    I("client/functions.lua")
+    I("client/hooks.lua")
+end
